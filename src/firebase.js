6696badc70971e1986cd1bfbf45c22b57a0e008b
danish <dannush786@gmail.com>
2021-07-29 17:19:43 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCpnCLFPYh5TT1RmBNwCHRKpKAiQc0REU4",
+    authDomain: "whatsapp-mern-3d766.firebaseapp.com",
+    projectId: "whatsapp-mern-3d766",
+    storageBucket: "whatsapp-mern-3d766.appspot.com",
+    messagingSenderId: "798777702183",
+    appId: "1:798777702183:web:29d02a46aebaf37d225b02"
+  };
